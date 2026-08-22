b44f9bae84060a9cec4000dbbbe724dd9a746bda
Hieu Nguyen (Jack) <nc.hieu@live.com>
2026-08-22 16:25:52 +0700

    Update CustomHeader.tsx

--- src/app/CustomHeader.tsx
+++ src/app/CustomHeader.tsx
@@ -1,5 +1,21 @@
 // This file is use for custom header, changes should not be commited to github or erased
 
 export default function CustomHeader() {
-  return null;
+  return (
+    /* @ts-expect-error */
+    <>
+      {/* @ts-expect-error */}
+      <script
+        defer
+        src="https://gumanalytics.com/js/analytics.js?v=be9add48f624"
+        data-domain="paymug.co"
+        data-session-replay="true"
+        data-replay-sample-rate="100"
+        data-heatmap="true"
+        data-heatmap-sample-rate="100"
+      >
+        {/* @ts-expect-error */}
+      </script>
+    </>
+  );
 }
